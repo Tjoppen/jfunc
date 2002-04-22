0111c60a128f20136cceee6754924f8f09d4e004
semios <semios>
2002-04-22 07:59:47 +0000

    converted dos2unix and added Version.java

---
+++ junit/extensions/jfunc/runner/Version.java
@@ -0,0 +1,14 @@
+package junit.extensions.jfunc.runner;   
+    
+/**   
+ * This class defines the current version of JFunc   
+ */   
+public class Version {   
+	private Version() {   
+		// don't instantiate   
+	}   
+
+	public static String id() {   
+		return "1.0";   
+	}   
+} 
